932f4d72f54707432df8cf29f886b30687a67b33
ajamarco <alexandre.j@gmail.com>
2017-09-20 21:11:36 -0300

    commit

--- app/src/main/java/com/example/jamarco/httpcapitulo8/MainActivity.java
+++ app/src/main/java/com/example/jamarco/httpcapitulo8/MainActivity.java
@@ -16,19 +16,38 @@ public class MainActivity extends AppCompatActivity {
 
         TextView a = (TextView) findViewById(R.id.textview);
 
+        //create a new ConnectivityManager which gives info about the device's connections
         ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
 
+        //get the active network
         NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
 
+        //if the type of network is WIFI
         if (activeNetwork.getType() == ConnectivityManager.TYPE_WIFI){
             a.setText("wifi connection");
         }
+        //if the type of network is mobile
         else if (activeNetwork.getType() == ConnectivityManager.TYPE_MOBILE){
             a.setText("mobile connection");
         }
+        //if the type of network is another type of connection
         else {
             a.setText("unknow connection");
         }
 
+    }
+
+    //check if there is some connection regardless if it's wifi, mobile, etc
+    public boolean hasConnection(){
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+
+        if (activeNetwork != null && activeNetwork.isConnected()){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 }
